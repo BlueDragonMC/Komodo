1afce9406c4ed1c59b595c2f4750ed263039412a
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2022-11-06 01:36:32 -0500

    Initial gRPC prototype (untested)

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,16 +11,26 @@ repositories {
     mavenCentral()
     maven(url = "https://repo.papermc.io/repository/maven-public/")
     maven(url = "https://jitpack.io")
+    mavenLocal()
 }
+
+val grpcKotlinVersion = "1.3.0"
+val protoVersion = "3.21.9"
+val grpcVersion = "1.50.2"
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("com.github.bluedragonmc:messagingsystem:3abc4b8a49")
-    implementation("com.github.bluedragonmc:messages:23a6e3bfc8")
     implementation("com.github.ben-manes.caffeine:caffeine:3.1.1")
     compileOnly("com.velocitypowered:velocity-api:3.1.1")
-    implementation("io.kubernetes:client-java:16.0.0")
+    implementation("io.kubernetes:client-java:16.0.1")
     kapt("com.velocitypowered:velocity-api:3.0.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+
+    runtimeOnly("io.grpc:grpc-netty:$grpcVersion")
+    implementation("io.grpc:grpc-stub:$grpcVersion")
+    implementation("io.grpc:grpc-kotlin-stub:$grpcKotlinVersion")
+    implementation("com.google.protobuf:protobuf-kotlin:$protoVersion")
+    implementation("com.bluedragonmc:rpc:1.0")
 }
 
 tasks["build"].dependsOn(tasks["shadowJar"])
