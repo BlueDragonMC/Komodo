8fb6b7b3612fdbd95e9c75ce0f3e21745252ca5e
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2025-03-27 16:46:24 -0400

    Update Velocity and Protocolize for 1.21.5

--- src/main/kotlin/com/bluedragonmc/komodo/ServerListPingHandler.kt
+++ src/main/kotlin/com/bluedragonmc/komodo/ServerListPingHandler.kt
@@ -74,7 +74,7 @@ class ServerListPingHandler {
             .favicon(favicon)
             .description(motd)
             .onlinePlayers(lastOnlinePlayerCount)
-            .version(Version(ProtocolVersion.MINECRAFT_1_21_4.protocol, "1.21.4"))
+            .version(Version(ProtocolVersion.MINECRAFT_1_21_5.protocol, "1.21.5"))
             .samplePlayers(*samplePlayers.toTypedArray())
             .build()
     }
